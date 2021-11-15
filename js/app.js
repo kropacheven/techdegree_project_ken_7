2a00bb43ae7931e52314aef488687ec7cc3d48d9
kropacheven <81347649+kropacheven@users.noreply.github.com>
2021-11-16 07:43:57 +0800

    Update1_16_11_2021
    Additional styling on members/activity, settings, message and alert-banner sections.

--- js/app.js
+++ js/app.js
@@ -6,7 +6,7 @@ const alertBanner = document.getElementById("alert");
 alertBanner.innerHTML =
 `
 <div class="alert-banner">
-<p><strong>Alert</strong>: You have unread messages_</p>
+<p class='alert-item'><strong>Alert</strong>: You have unread messages</p>
 <p class="alert-banner-close">x</p>
 </div>
 `;
