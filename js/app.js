22ebff91eeab1907252f777f31af9e912ecd6d9b
kropacheven <81347649+kropacheven@users.noreply.github.com>
2021-11-23 07:45:09 +0800

    Update1_23_11_2021
    Extra - autocomplete functions added

--- js/app.js
+++ js/app.js
@@ -232,6 +232,87 @@ for (let i = 0; i < trafficRadios.length; i++) {
 // 3. Add autocomplete feature for the "Search for a user box":
 
 
+const usernames = ['Victoria Chambers', 'Dale Byrd', 'Dawn Wood', 'Dan Oliver'];
+const userField = document.querySelector('#userField');
+const autocomplete = document.querySelector('.autocomplete');
+const autocompleteUl = document.querySelector('.autocomplete-users');
+let autocompleteChilds = document.querySelectorAll('.autocomplete-users li');
+
+const autocompleteEvent = userField.addEventListener('input', (e) => {
+    const userInput = userField.value.toLowerCase();
+    for (let i = 0; i < usernames.length; i++) {
+        const usernamesLowerCase = usernames[i].toLowerCase();
+        if (usernamesLowerCase.includes(userInput) & userInput !== '') {
+            addLi(usernames[i]);
+        } else {
+            removeLi(usernames[i]);
+        }
+    }
+    autocompleteChilds = document.querySelectorAll('.autocomplete-users li');
+    if (autocompleteChilds.length === 0) {
+        autocomplete.style.display = 'none';
+    } else {
+        autocomplete.style.display = 'block';
+    }
+});
+
+const autocompleteFocus = userField.addEventListener('focus', (e) => {
+    if (autocompleteChilds.length === 0) {
+        autocomplete.style.display = 'none';
+    } else {
+        autocomplete.style.display = 'block';
+    }
+});
+
+const autocompleteBlur = userField.addEventListener('blur', (e) => {
+    autocomplete.style.display = 'none';
+});
+
+const addLi = (name) => {
+    if (autocompleteChilds.length === 0) {
+        const li = document.createElement('LI');
+        li.innerHTML = name;
+        autocompleteUl.appendChild(li);
+    } else {
+        let x = 0;
+        for (let i = 0; i < autocompleteChilds.length; i++) {
+            const li = autocompleteChilds[i];
+            
+            if (li.innerHTML === name) {
+                x++;
+            }
+        }
+        if (x === 0) {
+            const li = document.createElement('LI');
+            li.innerHTML = name;
+            autocompleteUl.appendChild(li);        
+        } 
+    }
+};
+
+const removeLi = (name) => {
+    for (let i = 0; i < autocompleteChilds.length; i++) {
+        const li = autocompleteChilds[i];
+        if (li.innerHTML === name) {
+            li.remove();
+        } 
+    }
+};
+
+const autocompleteTypeName = autocompleteUl.addEventListener('click', (e) => {
+    const name = e.target.innerHTML;
+    userField.value = name;
+    for (let i = 0; i < autocompleteChilds.length; i++) {
+        const li = autocompleteChilds[i];
+        if (li.innerHTML !== name) {
+            li.remove();
+        } 
+    }
+});
+
+
+
+
 // 4. Use local storage to save the settings:
 
 // Grab buttons: 
